8b474a3905b4722863ec402df1c27b5a5407674f
eyob tariku <eyobtariku@gmail.com>
2019-12-31 16:05:50 +0300

    Add image to diplay when no transactions exist

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../models/transaction.dart';
+import './transaction_detail.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -10,66 +12,32 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 420,
-      child: ListView.builder(
-        itemBuilder: (ctx, idx) {
-          return Card(
-            margin: EdgeInsets.symmetric(
-              horizontal: 12,
-              vertical: 8,
-            ),
-            elevation: 4,
-            child: Container(
-              padding: EdgeInsets.only(
-                top: 10,
-                bottom: 10,
-              ),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(right: 8),
-                    padding: EdgeInsets.all(24),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      shape: BoxShape.circle,
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      '\$${transactions[idx].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'No transactions yet.',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  height: 240,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(bottom: 6),
-                        child: Text(
-                          transactions[idx].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                      ),
-                      Container(
-                        child: Text(
-                          transactions[idx].formattedDate,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, idx) => TransactionCard(transactions[idx]),
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
